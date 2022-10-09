fcaf178981cdecb8386e058f85cc7ccab6f7d8ce
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-10-09 10:12:27 +0600

    first commit

---
+++ src/features/counter/counterSlice.jsx
@@ -0,0 +1,14 @@
+import {createSlice} from "@reduxjs/toolkit";
+const initialState ={
+    count:0
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers:{
+        increment: (state)=>{
+
+        }
+    }
+})
